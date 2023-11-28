9f1a9ec956c54be75cf8ac072bed3b41248d9701
yk602 <85911868+epo2010@users.noreply.github.com>
2023-11-28 11:24:32 +0900

    2nd

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
       <h1 className="text-3xl font-bold">WebDev Topics</h1>
       <p className="mb-4">MongoDB CRUD Example</p>
       <p>Next-auth 인증</p>
-      {/*<TopicList />*/}
+      <TopicList />
     </>
   )
 }
